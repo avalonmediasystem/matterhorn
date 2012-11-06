a3b4d50f4991b257059761a0d0a1885e104efb55
Chris Colvard <cjcolvar@indiana.edu>
2012-11-06 10:48:35 -0500

    A totally new method and no logging yet

--- modules/matterhorn-ingest-service-impl/src/main/java/org/opencastproject/ingest/endpoint/IngestRestService.java
+++ modules/matterhorn-ingest-service-impl/src/main/java/org/opencastproject/ingest/endpoint/IngestRestService.java
@@ -395,7 +395,7 @@ public class IngestRestService {
       return Response.serverError().status(Status.INTERNAL_SERVER_ERROR).build();
     }
   }
-
+/**
   @POST
   @Produces(MediaType.TEXT_XML)
   @Consumes(MediaType.MULTIPART_FORM_DATA)
@@ -513,6 +513,7 @@ public class IngestRestService {
       return Response.serverError().status(Status.INTERNAL_SERVER_ERROR).build();
     }
   }
+*/
 
   @POST
   @Produces(MediaType.TEXT_XML)
@@ -520,63 +521,27 @@ public class IngestRestService {
   @Path("addMediaPackage/{wdID}")
   @RestQuery(name = "addMediaPackage", description = "Create media package from a media tracks and optional Dublin Core metadata fields", pathParameters = { @RestParameter(description = "Workflow definition id", isRequired = true, name = "wdID", type = RestParameter.Type.STRING) }, restParameters = {
           @RestParameter(description = "The kind of media track", isRequired = true, name = "flavor", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "abstract", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "accessRights", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "available", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "contributor", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "coverage", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "created", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "creator", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "date", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "description", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "extent", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "format", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "identifier", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "isPartOf", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "isReferencedBy", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "isReplacedBy", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "language", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "license", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "publisher", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "relation", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "replaces", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "rights", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "rightsHolder", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "source", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "spatial", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "subject", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "temporal", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "title", type = RestParameter.Type.STRING),
-          @RestParameter(description = "Metadata value", isRequired = false, name = "type", type = RestParameter.Type.STRING),
+          @RestParameter(description = "Metadata value", isRequired = true, name = "title", type = RestParameter.Type.STRING),
 	  @RestParameter(description = "The location of the media", isRequired = true, name = "url", type = RestParameter.Type.STRING)},
  reponses = {
           @RestResponse(description = "Returns augmented media package", responseCode = HttpServletResponse.SC_OK),
           @RestResponse(description = "", responseCode = HttpServletResponse.SC_BAD_REQUEST),
           @RestResponse(description = "", responseCode = HttpServletResponse.SC_INTERNAL_SERVER_ERROR) }, returnDescription = "")
-  public Response addMediaPackage(@Context HttpServletRequest request, @PathParam("wdID") String wdID, @FormParam("url") String url) {
+  public Response addMediaPackage(@FormParam("url") String url, @FormParam("flavor") String flavorStr, @FormParam("title") String title, @PathParam("wdID") String wdID) {
     MediaPackageElementFlavor flavor = null;
     try {
-      logger.debug("Inside addMediaPackage");
+      logger.info("Inside addMediaPackage");
       MediaPackage mp = ingestService.createMediaPackage();
       DublinCoreCatalog dcc = dublinCoreService.newInstance();
-      if (ServletFileUpload.isMultipartContent(request)) {
-        for (FileItemIterator iter = new ServletFileUpload().getItemIterator(request); iter.hasNext();) {
-          FileItemStream item = iter.next();
-          if (item.isFormField()) {
-            String fieldName = item.getFieldName();
-            if ("flavor".equals(fieldName)) {
-              flavor = MediaPackageElementFlavor.parseFlavor(Streams.asString(item.openStream()));
-	    } else {
-              // TODO not all form fields should be treated as dublin core fields
-              EName en = new EName(DublinCore.TERMS_NS_URI, fieldName);
-              dcc.add(en, Streams.asString(item.openStream()));
-            }
-          }
-        }
-        logger.debug("Inside addMediaPackage: " + flavor.toString());
-        logger.debug("Inside addMediaPackage: " + url);
+      flavor = MediaPackageElementFlavor.parseFlavor(flavorStr);
+
+       EName en = new EName(DublinCore.TERMS_NS_URI, "title");
+       dcc.add(en, title);
+
+        logger.info("Inside addMediaPackage: " + flavor.toString());
+        logger.info("Inside addMediaPackage: " + url);
         ingestService.addTrack(new URI(url), flavor, mp);
-        logger.debug("Inside addMediaPackage - added Track");
+        logger.info("Inside addMediaPackage - added Track");
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         dcc.toXml(out, true);
         InputStream in = new ByteArrayInputStream(out.toByteArray());
@@ -588,10 +553,8 @@ public class IngestRestService {
           workflow = ingestService.ingest(mp, wdID);
         }
         return Response.ok(workflow).build();
-      }
-      return Response.serverError().status(Status.BAD_REQUEST).build();
     } catch (Exception e) {
-      logger.debug("Inside addMediaPackage - catching exception");
+      logger.info("Inside addMediaPackage - catching exception");
       logger.warn("Inside addMediaPackage: " + e.getMessage(), e);
       return Response.serverError().status(Status.INTERNAL_SERVER_ERROR).build();
     }
